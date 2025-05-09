b27972e006c07a42261dd1eadb6463cb594c8d2f
Stefano Lusardi <stefano.lusardi@teiacare.com>
2025-05-09 15:25:43 +0200

    Added install_valgrind script

---
+++ tools/install_valgrind.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+VALGRIND_VERSION="3.20.0"
+CACHE_DIR="$1"
+COMPILER="$2"
+
+mkdir -p "$CACHE_DIR"
+
+# Check if valgrind is already installed from cache
+if [ -f "$CACHE_DIR/bin/valgrind" ]; then
+  echo "Using cached Valgrind"
+  sudo cp -r "$CACHE_DIR"/* /usr/local/
+else
+  echo "Installing Valgrind from source"
+  # Remove existing valgrind if present
+  sudo apt remove --purge valgrind -y
+
+  # Download and extract valgrind
+  cd "$(mktemp -d)"
+  wget https://sourceware.org/pub/valgrind/valgrind-$VALGRIND_VERSION.tar.bz2
+  tar xf valgrind-$VALGRIND_VERSION.tar.bz2
+
+  # Build and install valgrind
+  cd valgrind-$VALGRIND_VERSION
+  export CC=$COMPILER
+  ./configure --prefix=/usr/local
+  make -j$(nproc)
+  sudo make install
+
+  # Clean up
+  cd ..
+  rm -rf valgrind-$VALGRIND_VERSION valgrind-$VALGRIND_VERSION.tar.bz2
+
+  # Cache the installation
+  sudo cp -r /usr/local/bin "$CACHE_DIR"/
+  sudo cp -r /usr/local/lib "$CACHE_DIR"/
+  sudo cp -r /usr/local/include "$CACHE_DIR"/
+  sudo cp -r /usr/local/share "$CACHE_DIR"/
+  sudo chown -R "$(id -u):$(id -g)" "$CACHE_DIR"/
+fi
+
+# Verify installation
+# export PATH=/usr/local/bin:$PATH
+echo "Valgrind version:"
+valgrind --version
