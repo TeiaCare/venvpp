7df69b1a52583774b64d185a38ac0e733612eb7e
Stefano Lusardi <stefano.lusardi@teiacare.com>
2025-05-14 12:45:15 +0200

    Fixed environment variables for Address and Thread sanitizers.

--- tools/run_sanitizer.py
+++ tools/run_sanitizer.py
@@ -41,12 +41,12 @@ def sanitizer(args, program_args):
 def address_sanitizer(args, program_args):
     cmd = [ args.program_path ]
     cmd.extend(program_args)
-    run(cmd, env_vars={'TSAN_OPTIONS': f'exitcode=2 verbosity=1 log_path={args.tsan_log_path}'}, debug=True)
+    run(cmd, env_vars={'ASAN_OPTIONS': f'exitcode=2 verbosity=1 log_path={args.tsan_log_path}'}, debug=True)
 
 def thread_sanitizer(args, program_args):
     cmd = [ args.program_path ]
     cmd.extend(program_args)
-    run(cmd, env_vars={'ASAN_OPTIONS': f'exitcode=2 verbosity=1 log_path={args.asan_log_path}'}, debug=True)
+    run(cmd, env_vars={'TSAN_OPTIONS': f'exitcode=2 verbosity=1 log_path={args.asan_log_path}'}, debug=True)
 
 if __name__ == '__main__':
     args, program_args = parse()
