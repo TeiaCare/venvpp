60f5cfee5471eee5b4a87eccbc21cfd327b2d30a
Stefano Lusardi <stefano.lusardi@teiacare.com>
2025-08-05 16:53:10 +0200

    Bumped year to 2025

--- tools/run_doxygen.py
+++ tools/run_doxygen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #!/usr/bin/python
-# Copyright 2024 TeiaCare
+# Copyright 2025 TeiaCare
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
